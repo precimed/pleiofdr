be9ac67671b08c54b43acb92e0c154f800eab27d
zillurbmb51 <zillurbmb51@gmail.com>
2023-11-10 09:51:37 +0100

    Update conj_fuma_combined_lead.R

--- fuma/conj_fuma_combined_lead.R
+++ fuma/conj_fuma_combined_lead.R
@@ -2,21 +2,22 @@ print('This script would take 6 arguments, which are:')
 print('Conjuntional clump 0.05 lead csv, respective FUMA snps.tx, std sumstats for TRAIT1')
 print('std sumstats for TRAIT2, TRAIT1 name, TRAIT2 name')
 print('to produce conj_0.05_TRAIT1_vs_TRAIT2_lead.csv')
+library(data.table)
 args <- commandArgs(TRUE)
 #lead1=read.csv('fuma_input/conj.md_bmi.lead.csv',sep = '\t')
-lead1=read.csv(args[1],sep = '\t',stringsAsFactors=FALSE)
+lead1=fread(args[1],sep = '\t',stringsAsFactors=F,data.table=F)
 lead2=subset(lead1, is_locus_lead == "True")
 lead3=lead2[order(lead2$locusnum, lead2$FDR),]
 lead4 = lead3[!duplicated(lead3$locusnum),]
 lead5=subset(lead4, select = -c(is_locus_lead) )
 #snp1=read.csv('fuma_output_fin1/FUMA_conj_bmi_lead_job139938/snps.txt',sep = '\t')
-snp1=read.csv(args[2],sep = '\t',stringsAsFactors=FALSE)
+snp1=fread(args[2],sep = '\t',stringsAsFactors=F,data.table=F)
 leadsnp1=merge(lead5,snp1,by.x = 'LEAD_SNP',by.y = 'rsID')
 leadsnp2=leadsnp1[,c(2,3,1,4,5,6,7,12,11,18,19,20,21,22,23,24)]
 #sm1=read.table('sumstat-std/PGC_MD_2018_with23andMe_noUKBB.sumstats.gz',sep = '\t',header = T)
 #sm2=read.table('sumstat-std/GIANT_BMI_2015_EUR.sumstats.gz',sep = '\t',header = T)
-sm1=read.table(args[3],sep = '\t',header = T,stringsAsFactors=FALSE)
-sm2=read.table(args[4],sep = '\t',header = T,stringsAsFactors=FALSE)
+sm1=fread(args[3],sep = '\t',header = T,stringsAsFactors=F,data.table=F)
+sm2=fread(args[4],sep = '\t',header = T,stringsAsFactors=F,data.table=F)
 trait1=args[5]
 trait2=args[6]
 #trait1='DEP'
@@ -66,4 +67,4 @@ leadsnp4$ConcordEffect=ifelse(leadsnp4[[paste0('Z_recalculated_in_',trait1)]] > 
                               ifelse(leadsnp4[[paste0('Z_recalculated_in_',trait1)]] < 0 & leadsnp4[[paste0('Z_recalculated_in_',trait2)]] < 0,
                               'Yes','No'))
 
-write.csv(leadsnp4, file = paste0('conjFDR_0.05_',trait1,'_vs_',trait2, "_lead.csv"), sep='\t', row.names = FALSE)
+write.table(leadsnp4, file = paste0('conjFDR_0.05_',trait1,'_vs_',trait2, "_lead.csv"), sep='\t', row.names = F,quote=F)
